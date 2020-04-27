fef3da8fc6a3a3a9886da280a1d4a3b68141eeca
Ivan Maslov <cryptox3@gmail.com>
2020-04-27 13:40:47 +0300

    Get rid of 75kb lodash

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-import cloneDeep from 'lodash/cloneDeep';
+// import cloneDeep from 'lodash/cloneDeep';
+
+function cloneDeep(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return obj;
+    }
+
+    if (obj instanceof Date) {
+        return new Date(obj.getTime());
+    }
+
+    if (obj instanceof Array) {
+        return obj.reduce((arr, item, i) => {
+            arr[i] = deepCopy(item);
+            return arr;
+        }, []);
+    }
+
+    if (obj instanceof Object) {
+        return Object.keys(obj).reduce((newObj, key) => {
+            newObj[key] = deepCopy(obj[key]);
+            return newObj;
+        }, {})
+    }
+}
 
 const UNRESOLVED_LINK = {}; // unique object to avoid polyfill bloat using Symbol()
 
